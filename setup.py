ec41c4a41e983c2e58c5fc516a97a6a340611768
Alys Clark <acla148@bn405952.uoa.auckland.ac.nz>
2023-03-13 13:18:48 +1300

    basic directory structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='repro-image',
+    version='0.1.0',
+    packages=find_packages('source', exclude=['tests', 'tests.*', 'docs']),
+    package_dir={'': 'source'},
+    url='https://github.com/virtualpregnancy/repro-image.git',
+    license=license,
+    author='ABI Pregnancy Modelling Group',
+    author_email='alys.clark@auckland.ac.nz',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    description=''
+)
